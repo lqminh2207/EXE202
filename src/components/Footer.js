d09b48c932e28f71b972c09a0391c213e94769c7
Lê Quang Minh <lqminh2207@gmail.com>
2024-05-19 21:47:24 +0700

    a

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,7 +17,7 @@ const Footer = () => {
       <div className="max-w-screen-xl mx-auto grid grid-cols-4">
         <div className="flex flex-col gap-7">
           <img className="w-32" src={logoLight} alt="logoLight" />
-          <p className="text-white text-sm tracking-wide">© ReactBD.com</p>
+          <p className="text-white text-sm tracking-wide">© FPTU.com</p>
           <img className="w-56" src={paymentLogo} alt="paymentLogo" />
           <div className="flex gap-5 text-lg text-gray-400">
             <ImGithub className="hover:text-white duration-300 cursor-pointer" />
@@ -28,16 +28,16 @@ const Footer = () => {
           </div>
         </div>
         <div>
-          <h2 className="text-2xl font-semibold text-white mb-4">locate us</h2>
+          <h2 className="text-2xl font-semibold text-white mb-4">Locate us</h2>
           <div className="text-base flex flex-col gap-2">
-            <p>MBD,Ruwi, Muscat-Oman</p>
-            <p>Mobile: 00968 97859628</p>
-            <p>Phone: 00968 24769821</p>
-            <p>e-mail: bazar@gmail.com</p>
+            <p>FPT University</p>
+            <p>Mobile: 0368080707</p>
+            <p>Phone: 0368080707</p>
+            <p>e-mail: minhlqhe161101@fpt.edu.vn</p>
           </div>
         </div>
         <div>
-          <h2 className="text-2xl font-semibold text-white mb-4">profile</h2>
+          <h2 className="text-2xl font-semibold text-white mb-4">Profile</h2>
           <div className="text-base flex flex-col gap-2">
             <p className="flex items-center gap-3 hover:text-white duration-300 cursor-pointer">
               <span className="text-lg">
